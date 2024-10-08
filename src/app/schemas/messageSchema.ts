e47273f968e69dd00b99295a47ff433b74b1685d
Umar Farooq <ufservices465@gmail.com>
2024-10-08 20:03:56 +0530

    💠 zod schemas implemented

---
+++ src/app/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import {z} from 'zod'
+
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(10, {message:"Content must be atleast 10 characters"})
+    .max(300, {message:"Content must be atleast 10 characters"})
+})
---
+++ src/app/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+        .string()
+        .min(5,"Username must contain atleast 5 or more than that")
+        .max(20, "Username should not contain more than 20 characters")
+        .regex(/^[a-zA-Z0-9_]+$/, "Username must not containt any special character")
+
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email:z.string().email({message:"Invalid email address"}),
+    password:z.string().min(6, {message:"password must be atleast 6 characters"}),
+})
