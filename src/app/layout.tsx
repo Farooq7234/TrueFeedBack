1a3a3b97b3a1510d5c26419a1f192feca6a4b9c1
Umar Farooq <ufservices465@gmail.com>
2024-10-21 22:12:12 +0530

    next auth completed ✅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import AuthProvider from "@/context/authProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         {children}
       </body>
+      </AuthProvider>
     </html>
   );
 }
