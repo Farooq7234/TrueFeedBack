b7c227fd0befdb25a3077413c66d3c543b89bf4a
Umar Farooq <ufservices465@gmail.com>
2024-10-26 20:28:21 +0530

    message API completed

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,57 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect(); 
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  // session not found means below response will send
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  // Extracting id from user which comes from the session
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]).exec();
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        { message: "User not found", success: false },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      { messages: user[0].messages },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+    return Response.json(
+      { message: "Internal server error", success: false },
+      { status: 500 }
+    );
+  }
+}
