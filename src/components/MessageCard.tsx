25b532e1eba60fa9346a7e2395aa12a123f228d8
Umar Farooq <ufservices465@gmail.com>
2024-11-03 09:46:41 +0530

    Update MessageCard.tsx

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -36,7 +36,7 @@ export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
       toast({
         title: response.data.message,
       });
-      onMessageDelete(message._id);
+      onMessageDelete(message._id as string);
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
