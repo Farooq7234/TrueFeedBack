ee0bb570dc8f522c000c3c7d5d2daa46e11eec84
Umar Farooq <ufservices465@gmail.com>
2025-02-15 22:58:31 +0530

    fix: increase heading size in HeroSection for improved visibility

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,10 +7,10 @@ const HeroSection = () => {
   return (
     <div className="relative bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:24px_28px] ">
       {" "}
-      <section className=" min-h-screen flex justify-center items-center z-10 ">
+      <section className=" sm:min-h-screen flex justify-center items-center z-10 ">
         <div className="container mx-auto px-4">
           <div className="text-center max-w-3xl mx-auto">
-            <h1 className="text-2xl sm:text-5xl md:text-6xl font-bold tracking-tight">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight">
               Collect Anonymous Feedback <AuroraText>That Matters</AuroraText>
             </h1>
             <p className="text-lg text-muted-foreground mt-6">
